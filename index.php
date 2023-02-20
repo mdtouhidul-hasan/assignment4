48daa800de317970afa3eaf80f4537cada0f0479
touhidul hasan <hasanmd.touhidul05@gmail.com>
2023-02-20 19:41:18 +0600

    new line

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// assignment no 04
+
+// 1.Write a PHP function to sort an array of strings by their length, in ascending order. (Hint: You can use the usort() function to define a custom sorting function.)
